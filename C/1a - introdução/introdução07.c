804a7e41c710910956d71a37ab3433d6254f5ad1
Alan <alan.santosoliveirasp@gmail.com>
2024-01-16 01:46:56 -0300

    Delete

--- C/1a - introdução/introdução07.c
+++
@@ -1,9 +0,0 @@
-#include <stdio.h>
-
-int main(){
-	float salario;
-	printf("Digite o seu salário: ");
-	fflush(stdout);
-	scanf("De acordo com o reajuste, seu novo salário valerá R$%.2f.", salario*0.15);
-	return 0;
-}
